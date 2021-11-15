5e78f0c92254a699eac65756d069159e77885c39
M°ŋα Ł¹ſα <commit@only.lan>
2021-11-15 03:05:16 +0100

    2021-11-15-02.05

--- init.sh
+++ init.sh
@@ -375,7 +375,7 @@ done
         #            proxy_set_header       X-Templar-CacheFor '15m' ;
                     proxy_buffering        off;
                     error_log              /dev/stderr ;'
-        [[ "${ACCESS_LOG}" = "true" ]] &&  echo ' access_log             /dev/stdout upstream;' ;
+        [[ "${ACCESS_LOG}" = "true" ]] &&  echo ' access_log             /dev/stdout nocache;' ;
         [[ "${ACCESS_LOG}" = "true" ]] ||  echo ' access_log             off;' ;
 
         [[ "${HIDECLIENT}" = "true" ]] ||  echo '
